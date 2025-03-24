721aa1fa1c6dc32ef893f73e8fe8114937ae72ca
MariusFurter <80038150+MariusFurter@users.noreply.github.com>
2025-03-24 14:58:18 +0000

    Deploying to gh-pages from @ MariusFurter/mariusfurter.github.io@a67da24d503940221a8fd7e7adcebe7802a81515 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -56,16 +56,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2.html";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-just-returned-from-a-4-month-visit-with-giole-zardini-at-mit-will-miss-the-people-but-not-the-10-c-snowflake-snowman-snowflake",
+          title: 'Just returned from a 4-month visit with Giole Zardini at MIT. Will miss...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
